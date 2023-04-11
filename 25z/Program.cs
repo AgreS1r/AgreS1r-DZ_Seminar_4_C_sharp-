e3d609452a7bdbabc37a8ee503be24511b007530
danil <faceitplay2004@mail.ru>
2023-04-11 18:24:36 +0300

    exercise 25 27 29

---
+++ 25z/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter number A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Enter number B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{numberA}, {numberB} -> {Math.Pow(numberA, numberB)}");
